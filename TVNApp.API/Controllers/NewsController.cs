e8d2cb1c49d804ca5f4b7effef471caad89cb5e9
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-11-04 10:29:31 +0100

    Creating an endpoint which returns an xml content.

---
+++ TVNApp.API/Controllers/NewsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TVNApp.API.Controllers
+{
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/news")]
+        public ActionResult GetCurrentNews()
+        {
+            var client = new RestClient("http://www.tvn24.pl/najnowsze.xml");
+            var request = new RestRequest(Method.GET);
+
+            var response = client.Execute(request);
+
+            return Ok(response.Content);
+        }
+    }
+}
