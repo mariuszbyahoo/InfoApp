79708d534c5cd3e55ff3d8238c5f62729ffb58f8
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-11-04 17:33:15 +0100

    implementing the messages scroll

--- TVNApp.API/wwwroot/script.js
+++ TVNApp.API/wwwroot/script.js
@@ -6,3 +6,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+function showHideArticle(articleId) {
+    let el = document.getElementById(articleId);
+    if (el.style.height == "0%") {
+        el.style.height = "150px";
+        el.style.fontSize = "15px";
+    }
+    else {
+        el.style.height = "0%";
+        el.style.fontSize = "0%";
+    }
+}
